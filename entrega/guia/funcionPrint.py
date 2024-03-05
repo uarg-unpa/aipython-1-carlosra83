618d77d50f1044f70f59dfb21acb9693995208f5
carlosra83 <161720457+carlosra83@users.noreply.github.com>
2024-03-05 19:50:17 +0000

    clase condicionales

--- entrega/guia/funcionPrint.py
+++ entrega/guia/funcionPrint.py
@@ -23,3 +23,46 @@ print('soy linea 7')
 print(type(40))
 print((40+9))
 print((40+9)/2)
+
+# 05-03-2024
+# argumentos posicionales
+
+edad=18
+print(f"su edad es {edad}")
+
+# 
+texto="Esto eS uN texTo MeZclAdO"
+# title
+print(texto.title())
+print(texto)
+texto=texto.title()
+print(texto)
+# upper lower
+print(texto.upper())
+print(texto.lower())
+# repleace
+print(texto.replace("","?"))
+print(len(texto))
+
+# inciso h
+print("Calle: Libertad" "\t","\t" "Numero: 762","\t" "Codigo Postal: 9400")
+
+# sentencia if
+edad=int(input("ingrese su edad: "))
+if edad > 18:
+    print("es mayor de edad")
+    else:
+print("es menor de edad")
+# 
+calificacion=int(input("ingrese calificacion: "))
+if calificacion >=90:
+    print("Excelente")
+else: 
+   if calificacion >=80:
+      print("Muy bien")
+      else:
+      if calificacion >=70:
+         print("Bien")
+         else:
+         print("insuficiente")
+   
