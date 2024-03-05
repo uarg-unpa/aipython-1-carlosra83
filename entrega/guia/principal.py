02c23bdc9bb9d6657b804c908f52559ead6c7738
carlosra83 <161720457+carlosra83@users.noreply.github.com>
2024-03-05 13:43:34 +0000

    guia 1 reordenada

---
+++ entrega/guia/principal.py
@@ -0,0 +1,25 @@
+# Punto 1 utilizando la funcion print()
+# inciso a
+print("Las máquinas me sorprenden con mucha frecuencia")
+# inciso b
+print()
+# inciso c
+print("hola","soy","Carlos")
+#  inciso d prueba  
+print("23")
+print(23)
+# por mas que el 23 este o no con comillas me imprime el 23 sin comillas.
+# inciso e
+print("Una computadora puede ser llamada \"inteligente\"si logra engañar a una persona haciéndole creer que es un humano.")
+# inciso f
+print("Carlos","Raúl","Alvarez","40 años")
+# separador "sep=" y "end="
+print("carlos","Raúl","Alvarez","40 Años",sep="_")
+# separador "end="
+print("probando argumentos","soy el segundo argumento",end='-')
+print('soy otra linea 6')
+print('soy linea 7')
+# clases de daltos o literales
+print(type(40))
+print((40+9))
+print((40+9)/2)
