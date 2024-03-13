830a425c6b81d344c60ee5c4e0360a037e30733c
carlosra83 <161720457+carlosra83@users.noreply.github.com>
2024-03-13 15:45:17 +0000

    punto 5

--- entrega/guias/g1/ejercicio5a.py
+++ entrega/guias/g1/ejercicio5a.py
@@ -4,4 +4,4 @@ apellido = input("Por favor ingresa tu apellido: ")
 edad = input("Por favor ingresa tu edad: ")
 
 # Mostrar mensaje creativo con los datos ingresados
-print(f"{nombre} {apellido}, de {edad} años, ¡sé creativo!")
+print (f"{nombre} {apellido}, de {edad} años, ¡sé creativo!")
--- entrega/guias/g1/ejercicio5b.py
+++ entrega/guias/g1/ejercicio5b.py
@@ -3,10 +3,6 @@ nombre = input("Por favor ingresa tu nombre: ")
 apellido = input("Por favor ingresa tu apellido: ")
 edad = input("Por favor ingresa tu edad: ")
 
-# Solicitar nombre, apellido y edad al usuario
-nombre = input("Por favor ingresa tu nombre: ")
-apellido = input("Por favor ingresa tu apellido: ")
-edad = input("Por favor ingresa tu edad: ")
-
 # Construir el mensaje creativo
 mensaje = f"{nombre} {apellido}, de {edad} años, ¡sé creativo!"
+input(mensaje)
