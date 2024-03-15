9f7dfb320c2e3482d6b95476b6d9f0bd9cf3ba7c
carlosra83 <161720457+carlosra83@users.noreply.github.com>
2024-03-15 19:51:09 +0000

    fin1503

--- entrega/guias/g1/ejercicio12.py
+++ entrega/guias/g1/ejercicio12.py
@@ -1,8 +1,12 @@
 # Escribir un programa que pregunte al usuario una cantidad a invertir, el interés anual
 # y el número de años, y muestre por pantalla el capital obtenido en la inversión.
 
-Invertir= float(input("Ingrese cuanto desea invertir: "))
+invertir= float(input("Ingrese cuanto desea invertir: "))
 
 interesAnual= float(input("ingrease el interes anual: "))
 
-tiempoInversion= int(input("ingrese la cantidad de meses a invertir"))
+tiempoInversion= int(input("ingrese la cantidad de años a invertir. "))
+
+inversion= invertir*(interesAnual/100)*tiempoInversion
+
+print("su inversion obtenida es de: ", inversion)
