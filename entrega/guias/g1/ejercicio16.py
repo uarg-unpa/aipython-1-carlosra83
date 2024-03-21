c3be651efaea39d6e9f05d0f094a86a91f5c95df
carlosra83 <161720457+carlosra83@users.noreply.github.com>
2024-03-21 16:43:42 +0000

    16

---
+++ entrega/guias/g1/ejercicio16.py
@@ -0,0 +1,14 @@
+texto = "sometimes it is the people no one imagines anything of who do the things that no one can imagine."
+
+# imprime en mayuscula solamente la primer letra de toda la linea
+capitalized_texto = texto.capitalize()
+
+# imprime en mayusculas las primeras letras de cada palabra
+title_texto = texto.title()
+
+# imprime todo en mayuscula
+swapped_texto = texto.swapcase()
+
+print("Capitalized:", capitalized_texto)
+print("Titled:", title_texto)
+print("Swapped Case:", swapped_texto)
